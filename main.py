8cc331b535d465a701e52b0700c108001f2f8e44
KOUSHANI <koushaniot7@gmail.com>
2026-08-10 17:13:05 +0530

    Fix CORS middleware - backend ready

--- main.py
+++ main.py
@@ -13,10 +13,15 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Auth Platform", version="1.0.0")
 
-# CORS configuration
+# cors
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5500" , "https://authenticatingxyz-frontend.onrender.com/"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:5500",
+        "https://authenticatingxyz-frontend.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
