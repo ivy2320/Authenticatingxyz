247d47ff5a974bb7c154e4b94e117d3d02c01687
KOUSHANI <koushaniot7@gmail.com>
2026-08-10 17:00:26 +0530

    Add Vercel URL to CORS

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Auth Platform", version="1.0.0")
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5500" , "https://authenticatingxyz-frontend.onrender.com"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5500" , "https://authenticatingxyz-frontend.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
