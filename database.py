8281e1cebf2728463e9a7b897c1915cd65e78160
KOUSHANI <koushaniot7@gmail.com>
2026-08-25 13:10:54 +0530

    prevented timeouts

--- database.py
+++ database.py
@@ -6,8 +6,16 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=5,
+    max_overflow=10,
+    pool_timeout=30,
+    pool_recycle=1800,  # recycle connections every 30 min
+    pool_pre_ping=True,  # check connection health before using
+)
 
-engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
